7e8844b190e07b9f9a744d192979d12b63e8f34c
Chris Wong <wongchris@google.com>
2015-12-16 10:34:56 +1100

    Add scripts for running fuzz1 experiment

---
+++ fuzz1/collect.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+chromium_src="$HOME/chromium/src"
+
+../node_modules/erlenmeyer/erlnmyr collect.erlnmyr --chromium="$chromium_src"
---
+++ fuzz1/process.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+../node_modules/erlenmeyer/erlnmyr process.erlnmyr --input=traces > summary.csv
