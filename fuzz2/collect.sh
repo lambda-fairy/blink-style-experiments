95e1232dcb128499ccecce4a07770784db768c25
Chris Wong <wongchris@google.com>
2015-12-16 10:28:57 +1100

    Organize files into a structure that makes sense

--- fuzz2/collect.sh
+++ fuzz2/collect.sh
@@ -8,10 +8,10 @@ run() {
     "$@"
 }
 
-for json_input in fuzz2-tests/*
+for json_input in experiment-specs/*
 do
     experiment_name="$(basename "$json_input" .json)"
     output_dir="traces/$experiment_name"
-    run node_modules/erlenmeyer/erlnmyr fuzz2.erlnmyr --chromium="$chromium_src" \
+    run ../node_modules/erlenmeyer/erlnmyr collect.erlnmyr --chromium="$chromium_src" \
         --input="$json_input" --output="$output_dir"'/$1-$2-$3-$4-$5.trace'
 done
--- fuzz2/process.sh
+++ fuzz2/process.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-for json_input in fuzz2-tests/*
+for json_input in experiment-specs/*
 do
     experiment_name="$(basename "$json_input" .json)"
     output_dir="traces/$experiment_name"
-    > "fuzz2-$experiment_name.csv" \
-        node_modules/erlenmeyer/erlnmyr fuzz2-process.erlnmyr \
+    > "summary-$experiment_name.csv" \
+        ../node_modules/erlenmeyer/erlnmyr process.erlnmyr \
         --input="$output_dir" --xvariable="$experiment_name"
 done
